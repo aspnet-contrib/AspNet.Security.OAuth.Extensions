dfd60e928f95c7466169028f9849b2086b349c00
Kévin Chalet <kevinchalet@gmail.com>
2015-12-09 00:32:46 +0100

    Add license headers

--- src/Owin.Security.OAuth.Introspection/OAuthIntrospectionDefaults.cs
+++ src/Owin.Security.OAuth.Introspection/OAuthIntrospectionDefaults.cs
@@ -1,4 +1,10 @@
-﻿namespace Owin.Security.OAuth.Introspection {
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Extensions for more information
+ * concerning the license and the contributors participating to this project.
+ */
+
+namespace Owin.Security.OAuth.Introspection {
     public static class OAuthIntrospectionDefaults {
         /// <summary>
         /// Gets the default scheme used by the introspection middleware.
